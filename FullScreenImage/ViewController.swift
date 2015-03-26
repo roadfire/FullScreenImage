3e9114a00154f0a278ea441871be1affcb9d5ac1
Josh Brown <josh@roadfiresoftware.com>
2015-03-25 20:22:57 -0400

    Initial Commit

---
+++ FullScreenImage/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  FullScreenImage
+//
+//  Created by Josh Brown on 3/25/15.
+//  Copyright (c) 2015 Roadfire Software. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
